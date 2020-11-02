74c3951fa0476ae63cb9b19defdbb9a54c41b071
= <=>
2020-11-02 12:19:47 +0100

    Basic Map added

---
+++ core/assets/map/basic/basic.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="basic" tilewidth="32" tileheight="32" spacing="2" margin="2" tilecount="3" columns="3">
+ <image source="../../packed/map/atlas.png" width="128" height="64"/>
+</tileset>
